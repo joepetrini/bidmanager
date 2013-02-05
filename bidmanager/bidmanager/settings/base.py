964e8a9d448c285e9f93aebca5191d8dc4e59931
Joe Petrini <=>
2013-02-04 22:54:09 -0500

    Login form and base templates added

--- bidmanager/bidmanager/settings/base.py
+++ bidmanager/bidmanager/settings/base.py
@@ -109,7 +109,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'profiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
