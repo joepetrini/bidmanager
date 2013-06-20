e2c4fc9c4303cd6d283859a5ff90a30d8eaa922f
Joe Petrini <joepetrini@gmail.com>
2013-06-20 12:00:37 -0400

    Added pagination to search

--- bidmanager/bidmanager/settings/base.py
+++ bidmanager/bidmanager/settings/base.py
@@ -133,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.markup',
     'haystack',
+    'endless_pagination',
     'profiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
